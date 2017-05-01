db02bb04e8cbfa13bb7479f8cb2677d7d9affbd9
andrewgurung <andrewgurungg@gmail.com>
2017-05-01 19:05:47 -0400

    Firebase setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
 
+import Firebase from 'firebase '
+
+let config = {
+  apiKey: "AIzaSyBBOM9pZwE_KdRVCbZjLANWAv-E-5cpB7g",
+  authDomain: "vue-firebase-spa.firebaseapp.com",
+  databaseURL: "https://vue-firebase-spa.firebaseio.com",
+  projectId: "vue-firebase-spa",
+  storageBucket: "vue-firebase-spa.appspot.com",
+  messagingSenderId: "529425484072"
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.datebase();
+
+let booksRef = db.ref('books');
+
 export default {
   name: 'app',
-  components: {
-    Hello
+  firebase: {
+    books: booksRef
   }
 }
 </script>
